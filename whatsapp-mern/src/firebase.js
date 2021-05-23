c17807d1beb3e3e788dd0c7ce2382f16bc861951
Prakhar Sharma <56041320+Prakharsharma2206@users.noreply.github.com>
2021-05-23 21:01:21 +0530

    Frontend

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAlaEzzN0Ulp2wx4giKttzZs4mcTvFO608",
+    authDomain: "whatsapp-mern-4aa76.firebaseapp.com",
+    projectId: "whatsapp-mern-4aa76",
+    storageBucket: "whatsapp-mern-4aa76.appspot.com",
+    messagingSenderId: "240702504607",
+    appId: "1:240702504607:web:232ad2d3f6030f76492caa",
+    measurementId: "G-8X7WBRY7Q8"
+  };
